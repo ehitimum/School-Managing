00f763ffe0285e208a398d78bd8670815ab89894
ehitimum <ehitimumchoudhury@gmail.com>
2023-07-31 14:23:04 +0600

    ADD: 1.0

---
+++ src/main/java/com/example/school_managing/SchoolManagingApplication.java
@@ -0,0 +1,13 @@
+package com.example.school_managing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolManagingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/school_managing/SchoolManagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.school_managing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

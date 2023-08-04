2f61fe622082ef2427cd694a92e9c7ec28c61aee
ehitimum <ehitimumchoudhury@gmail.com>
2023-08-04 11:51:41 +0600

    ADD: 1.5

--- src/test/java/com/example/school_managing/repository/CourseRepositoryTest.java
+++ src/test/java/com/example/school_managing/repository/CourseRepositoryTest.java
@@ -1,7 +1,25 @@
 package com.example.school_managing.repository;
 
+import com.example.school_managing.entity.Course;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class CourseRepositoryTest {
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @Test
+    public void printCourses(){
+        List<Course> courseList = courseRepository.findAll();
+
+        System.out.println("courseList = " + courseList);
+    }
+
 
 }
